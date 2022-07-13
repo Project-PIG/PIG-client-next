b29cc5e7e8431dbda2e124f1ab5880dc9ba23b83
minjekang <kangminje777@gmail.com>
2022-07-13 14:16:12 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import type { NextPage } from "next";
+import Header from "../components/Header";
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <div>ss</div>
-    </div>
+    <>
+      <Header />
+    </>
   );
 };
 
