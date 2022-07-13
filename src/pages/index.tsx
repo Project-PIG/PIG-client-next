28bd1698a2f546bd12625dbb50fa5bd263f0acd8
minjekang <kangminje777@gmail.com>
2022-07-13 23:37:40 +0900

    컨텐츠 컴포넌트 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import type { NextPage } from "next";
 import Header from "../components/Header";
-
+import Contents from "../components/Contents";
 const Home: NextPage = () => {
   return (
     <>
       <Header />
+      <Contents />
     </>
   );
 };
